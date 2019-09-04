0ec5a5eaea4ce9da77e439a7d3454d5ccebecd3e
garybot <argot.pryer@gmail.com>
2019-09-03 21:42:45 -0700

    Completed Task 1

--- js/index.js
+++ js/index.js
@@ -40,3 +40,48 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// nav
+const navItems = document.querySelectorAll('a');
+navItems[0].textContent = siteContent.nav["nav-item-1"];
+navItems[1].textContent = siteContent.nav["nav-item-2"];
+navItems[2].textContent = siteContent.nav["nav-item-3"];
+navItems[3].textContent = siteContent.nav["nav-item-4"];
+navItems[4].textContent = siteContent.nav["nav-item-5"];
+navItems[5].textContent = siteContent.nav["nav-item-6"];
+
+// cta
+document.querySelector('.cta-text h1').textContent = siteContent.cta.h1;
+document.querySelector('.cta-text button').textContent = siteContent.cta.button;
+document.getElementById('cta-img').src = siteContent['cta']['img-src'];
+
+// main
+
+const headings = document.querySelectorAll('.text-content h4');
+headings[0].textContent = siteContent["main-content"]["features-h4"];
+headings[1].textContent = siteContent["main-content"]["about-h4"];
+headings[2].textContent = siteContent["main-content"]["services-h4"];
+headings[3].textContent = siteContent["main-content"]["product-h4"];
+headings[4].textContent = siteContent["main-content"]["vision-h4"];
+
+const content = document.querySelectorAll('.text-content p');
+content[0].textContent = siteContent["main-content"]["features-content"];
+content[1].textContent = siteContent["main-content"]["about-content"];
+content[2].textContent = siteContent["main-content"]["services-content"];
+content[3].textContent = siteContent["main-content"]["product-content"];
+content[4].textContent = siteContent["main-content"]["vision-content"];
+
+document.getElementById('middle-img').src = siteContent["main-content"]["middle-img-src"];
+
+// contact
+
+const contactHeader = document.querySelector(".contact h4");
+contactHeader.textContent = siteContent.contact["contact-h4"];
+const addressFields = document.querySelectorAll(".contact p");
+addressFields[0].textContent = siteContent.contact.address;
+addressFields[1].textContent = siteContent.contact.phone;
+addressFields[2].textContent = siteContent.contact.email;
+
+// footer 
+
+document.querySelector('footer p').textContent = siteContent.footer.copyright;
