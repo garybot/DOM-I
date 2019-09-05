be67a30c891f8d60762216dc13606f1e8a324c81
garybot <argot.pryer@gmail.com>
2019-09-04 18:24:00 -0700

    Added a bit of style to the timer.

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -7,38 +7,38 @@ const digits = document.querySelector('.digits');
 let start;
 const startButton = document.querySelector('.start');
 startButton.addEventListener('click', () => {
-	start = window.setInterval(timer, 10);
-	startButton.disabled = "true";
+  start = window.setInterval(timer, 10);
+  startButton.disabled = "true";
 });
 
 const resetButton = document.querySelector('.reset');
 resetButton.addEventListener('click', () => {
-	clearInterval(start);
-	digits.classList.remove('redDigit');
-	startButton.disabled = '';
-	secondTens.textContent = "0";
-	secondOnes.textContent = "0";
-	msTens.textContent = "0";
-	msHundreds.textContent = "0";
+  clearInterval(start);
+  digits.classList.remove('redDigit');
+  secondTens.textContent = "0";
+  secondOnes.textContent = "0";
+  msTens.textContent = "0";
+  msHundreds.textContent = "0";
+  startButton.disabled = '';
 });
 
 function timer() {
-	if (Number(msTens.textContent) < 9) {
-		msTens.textContent = Number(msTens.textContent) + 1;
-	} else {
-		msTens.textContent = "0";
-		if (Number(msHundreds.textContent) < 9) {
-			msHundreds.textContent = Number(msHundreds.textContent) + 1;
-		} else {
-			msHundreds.textContent = "0";
-			if (Number(secondOnes.textContent) < 9) {
-				secondOnes.textContent = Number(secondOnes.textContent) + 1;
-			} else {
-				clearInterval(start);
-				secondOnes.textContent = "0";
-				secondTens.textContent = "1";
-				digits.classList.add("redDigit");
-			}
-		}
-	}
+  if (Number(msTens.textContent) < 9) {
+    msTens.textContent = Number(msTens.textContent) + 1;
+  } else {
+    msTens.textContent = "0";
+    if (Number(msHundreds.textContent) < 9) {
+      msHundreds.textContent = Number(msHundreds.textContent) + 1;
+    } else {
+      msHundreds.textContent = "0";
+      if (Number(secondOnes.textContent) < 9) {
+        secondOnes.textContent = Number(secondOnes.textContent) + 1;
+      } else {
+        clearInterval(start);
+        secondOnes.textContent = "0";
+        secondTens.textContent = "1";
+        digits.classList.add("redDigit");
+      }
+    }
+  }
 }
